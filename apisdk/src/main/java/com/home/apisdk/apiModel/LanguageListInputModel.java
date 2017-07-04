b92f1c08fdaf25b1c22dd9ddd04272e27443a377
alok1984 <alok.oca@gmail.com>
2017-07-04 12:49:00 +0530

    translation language api implemented

--- apisdk/src/main/java/com/home/apisdk/apiModel/LanguageListInputModel.java
+++ apisdk/src/main/java/com/home/apisdk/apiModel/LanguageListInputModel.java
@@ -8,6 +8,15 @@ public class LanguageListInputModel {
 
     String authToken;
 
+    public String getLangCode() {
+        return langCode;
+    }
+
+    public void setLangCode(String langCode) {
+        this.langCode = langCode;
+    }
+
+    String langCode;
 
     public void setAuthToken(String authToken){
         this.authToken = authToken;
