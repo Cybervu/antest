febd4d561d6b04d0ab186ff47d9920f04ab5c17a
alok1984 <alok.oca@gmail.com>
2017-08-17 14:12:21 +0530

    newthoughtchannel flavoured added

--- app/src/main/java/com/home/vod/util/Constant.java
+++ app/src/main/java/com/home/vod/util/Constant.java
@@ -14,7 +14,7 @@ public class Constant {
     public static final String CENSOR_RATING_INTENT_KEY = "CENSORRATING";
     public static final String CAST_INTENT_KEY = "CAST";
     public static final String SEASON_INTENT_KEY = "SEASON";
-    public static final String authTokenStr = "6a9c6e9bfeee69e63d4bab668c01dc0a"; //monica studio
+    public static final String authTokenStr = "4298851905443517c9f161e6b0471969"; //monica studio
     public static final String VIDEO_TITLE_INTENT_KEY = "VIDEO TITLE";
 
 }
