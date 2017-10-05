fbb1efca08b16303a1af514cc8a2ecc744a5fd7a
Anuradha <anuradha@muvi.com>
2017-10-05 16:44:54 +0530

    categories added to nav drawer

--- app/src/main/java/com/home/vod/adapter/RecyclerViewDataAdapter.java
+++ app/src/main/java/com/home/vod/adapter/RecyclerViewDataAdapter.java
@@ -272,11 +272,11 @@ public  class RecyclerViewDataAdapter extends RecyclerView.Adapter<RecyclerViewD
 
             if(!firstTime){
                 firstTime = true;
-              /*  //for dynamic banner
-               loadDynamicBanners(mDemoSlider,view,this);*/
+                //for dynamic banner
+                loadDynamicBanners(mDemoSlider,view,this);
 
                // for static banner
-               loadStaticBanners(mDemoSlider,view,this);
+            //   loadStaticBanners(mDemoSlider,view,this);
 
             }
 
