f973f018ed1f6eafb3326fef1ec916bf7bbb04cd
Anuradha <anuradha@muvi.com>
2017-11-01 14:54:59 +0530

    cmax added

---
+++ app/src/cmax/java/com/home/vod/RegisterUIHandler.java
@@ -0,0 +1,78 @@
+package com.home.vod;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.facebook.CallbackManager;
+import com.home.vod.preferences.LanguagePreference;
+import com.home.vod.preferences.PreferenceManager;
+import com.home.vod.util.FontUtls;
+import com.home.vod.util.LogUtil;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static com.facebook.FacebookSdk.getApplicationContext;
+import static com.home.vod.preferences.LanguagePreference.AGREE_TERMS;
+import static com.home.vod.preferences.LanguagePreference.DEFAULT_AGREE_TERMS;
+import static com.home.vod.preferences.LanguagePreference.DEFAULT_TERMS;
+import static com.home.vod.preferences.LanguagePreference.TERMS;
+
+/**
+ * Created by BISHAL on 21-08-2017.
+ */
+
+public class RegisterUIHandler {
+    private Activity context;
+    private TextView termsTextView,termsTextView1;
+    private LinearLayout btnLogin;
+    public  String selected_Language_Id="", selected_Country_Id="";
+    private Button loginButton;
+    private LanguagePreference languagePreference;
+
+    public RegisterUIHandler(Activity context){
+        this.context=context;
+        termsTextView = (TextView) context.findViewById(R.id.termsTextView);
+        termsTextView1 = (TextView) context.findViewById(R.id.termsTextView1);
+        btnLogin = (LinearLayout) context.findViewById(R.id.btnLogin);
+        btnLogin.setVisibility(View.GONE);
+    }
+    public void setCountryList(PreferenceManager preferenceManager){
+
+
+    }
+    public void setTermsTextView(LanguagePreference languagePreference){
+        termsTextView1.setText(languagePreference.getTextofLanguage(AGREE_TERMS, DEFAULT_AGREE_TERMS));
+        termsTextView.setText(languagePreference.getTextofLanguage(TERMS, DEFAULT_TERMS));
+
+
+        termsTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://cmaxtv.com/politicas-de-privacidad"));
+                browserIntent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                context.startActivity(browserIntent);
+            }
+        });
+   }
+
+
+    public void callFblogin(final CallbackManager callbackManager, Button loginButton, LanguagePreference languagePreference){
+
+    }
+
+    public void callSignin(LanguagePreference languagePreference){
+
+    }
+
+}
