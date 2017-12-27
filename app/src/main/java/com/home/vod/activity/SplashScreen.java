05fba74f5b58fc38aa231ebd8a5bf06e2b3474e1
Muvidevelopers <abhishek@muvi.com>
2017-12-27 19:53:20 +0530

    Mandis #11487
    Euroman Flavor build
    Euroman UI set including colors and drawables
    Unit Testing Done
    build submitted to QA folder and status changed to QA Ready

--- app/src/main/java/com/home/vod/activity/SplashScreen.java
+++ app/src/main/java/com/home/vod/activity/SplashScreen.java
@@ -127,12 +127,12 @@ public class SplashScreen extends Activity implements GetIpAddressAsynTask.IpAdd
         float dpHeight = displayMetrics.heightPixels / displayMetrics.density;
         float dpWidth = displayMetrics.widthPixels / displayMetrics.density;*/
 
-        Display display = getWindowManager().getDefaultDisplay();
+        /*Display display = getWindowManager().getDefaultDisplay();
         float dpHeight = display.getHeight();
         float dpWidth = display.getWidth();
         imageResize.setImageBitmap(decodeSampledBitmapFromResource(getResources(), R.drawable.splash_screen, dpWidth, dpHeight));
         if ( Util.isTablet(SplashScreen.this)){
-            imageResize.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageResize.setScaleType(ImageView.ScaleType.FIT_XY);
         }else {
             imageResize.setScaleType(ImageView.ScaleType.FIT_XY);
             try {
@@ -141,7 +141,14 @@ public class SplashScreen extends Activity implements GetIpAddressAsynTask.IpAdd
 
             }
 
-        }
+        }*/
+
+        Display display = getWindowManager().getDefaultDisplay();
+        float dpHeight = display.getHeight();
+        float dpWidth = display.getWidth();
+        imageResize.setImageBitmap(decodeSampledBitmapFromResource(getResources(), R.drawable.splash_screen, dpWidth, dpHeight));
+        splashScreenHandler.handleSplashscreen(imageResize);
+
 
 
         noInternetTextView.setText(languagePreference.getTextofLanguage(NO_INTERNET_CONNECTION, DEFAULT_NO_INTERNET_CONNECTION));
