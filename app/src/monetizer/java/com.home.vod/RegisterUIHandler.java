00ee09d4aa7df2445961a3d1fa22426cecb88e68
pratikjoshi999 <pratikjoshi999@gmail.com>
2018-02-27 14:10:07 +0530

    @author- Kushal Status: Register Activity: First Name and Last Name

--- app/src/monetizer/java/com.home.vod/RegisterUIHandler.java
+++ app/src/monetizer/java/com.home.vod/RegisterUIHandler.java
@@ -30,6 +30,8 @@ import static com.facebook.FacebookSdk.getApplicationContext;
 import static com.home.vod.preferences.LanguagePreference.AGREE_TERMS;
 import static com.home.vod.preferences.LanguagePreference.DEFAULT_AGREE_TERMS;
 import static com.home.vod.preferences.LanguagePreference.DEFAULT_ENTER_REGISTER_FIELDS_DATA;
+import static com.home.vod.preferences.LanguagePreference.DEFAULT_FIRST_NAME;
+import static com.home.vod.preferences.LanguagePreference.DEFAULT_LAST_NAME;
 import static com.home.vod.preferences.LanguagePreference.DEFAULT_NAME_HINT;
 import static com.home.vod.preferences.LanguagePreference.DEFAULT_TERMS;
 import static com.home.vod.preferences.LanguagePreference.ENTER_REGISTER_FIELDS_DATA;
@@ -45,7 +47,11 @@ public class RegisterUIHandler {
     private TextView termsTextView,termsTextView1;
     private LinearLayout btnLogin;
     private EditText editName;
+    // Kushal
+    private EditText editNameFirst, editNameLast, editEmail, editPassword, editConfirmPassword;
+    //
     public  String selected_Language_Id="", selected_Country_Id="",regNameStr,regPhone="",last_name="";
+    public String regFirstNameStr,regLastNameStr;
     private LanguagePreference languagePreference;
 
     public RegisterUIHandler(Activity context){
@@ -54,7 +60,11 @@ public class RegisterUIHandler {
         termsTextView1 = (TextView) context.findViewById(R.id.termsTextView1);
         btnLogin = (LinearLayout) context.findViewById(R.id.btnLogin);
         btnLogin.setVisibility(View.GONE);
-        editName = (EditText) context.findViewById(R.id.editNameStr);
+       // editName = (EditText) context.findViewById(R.id.editNameStr);
+        // Kushal
+        editNameFirst = (EditText) context.findViewById(R.id.editNameStr_first);
+        editNameLast= (EditText)context.findViewById(R.id.editNameStr_last);
+        //
         languagePreference = LanguagePreference.getLanguagePreference(context);
 
 
@@ -73,8 +83,10 @@ public class RegisterUIHandler {
     public void setTermsTextView(LanguagePreference languagePreference){
         termsTextView1.setText(languagePreference.getTextofLanguage(AGREE_TERMS, DEFAULT_AGREE_TERMS));
         termsTextView.setText(languagePreference.getTextofLanguage(TERMS, DEFAULT_TERMS));
-        FontUtls.loadFont(context, context.getResources().getString(R.string.light_fonts), editName);
-        editName.setHint(languagePreference.getTextofLanguage(NAME_HINT, DEFAULT_NAME_HINT));
+        FontUtls.loadFont(context, context.getResources().getString(R.string.light_fonts), editNameFirst);
+        FontUtls.loadFont(context, context.getResources().getString(R.string.light_fonts), editNameLast);
+        editNameFirst.setHint(languagePreference.getTextofLanguage(NAME_HINT, DEFAULT_FIRST_NAME));
+        editNameLast.setHint(languagePreference.getTextofLanguage(NAME_HINT, DEFAULT_LAST_NAME));
 
         termsTextView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -86,9 +98,13 @@ public class RegisterUIHandler {
         });
    }
     public void getRegisterName(){
-        regNameStr = editName.getText().toString().trim();
-        if (!regNameStr.equals("")) {
-            ((RegisterActivity) context).registerButtonClicked(regNameStr,last_name,regPhone);
+       // regNameStr = editName.getText().toString().trim();
+        //Kushal
+        regFirstNameStr= editNameFirst.getText().toString().trim();
+        regLastNameStr= editNameLast.getText().toString().trim();
+        //
+        if (!regFirstNameStr.equals("") && !regLastNameStr.equals("") || !regLastNameStr.isEmpty() && !regFirstNameStr.isEmpty()) {
+            ((RegisterActivity) context).registerButtonClicked(regFirstNameStr,regLastNameStr,regPhone);
         }else {
             Toast.makeText(context, languagePreference.getTextofLanguage(ENTER_REGISTER_FIELDS_DATA, DEFAULT_ENTER_REGISTER_FIELDS_DATA), Toast.LENGTH_LONG).show();
         }
