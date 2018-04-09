5e2b3ee89cc1602d3835c31c634f259a51020957
BISHAL <bishalseth2012@gmail.com>
2018-04-09 11:21:53 +0530

    @Author:Bishal status:completed desc:Resizable custom textview viewmore viewless issue solved and yesflix issue resolved

--- app/src/main/java/com/home/vod/fragment/MyLibraryFragment.java
+++ app/src/main/java/com/home/vod/fragment/MyLibraryFragment.java
@@ -19,6 +19,7 @@ import android.os.Environment;
 import android.os.Handler;
 import android.os.Parcelable;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
@@ -392,6 +393,12 @@ public class MyLibraryFragment extends Fragment implements VideoDetailsAsynctask
             mDemoSlider.stopAutoCycle();
         }
         super.onStop();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        setReenterTransition(true);
+        super.onCreate(savedInstanceState);
     }
 
     @Override
