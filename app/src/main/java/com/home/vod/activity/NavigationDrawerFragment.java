d9e93c256dc873b1ca9d12176b9ff6e83808c2c7
Muvidevlopers <kushalmahapatro@gmail.com>
2018-04-10 11:34:23 +0530

    @author Kushal picking google-services.json from individual flavors

--- app/src/main/java/com/home/vod/activity/NavigationDrawerFragment.java
+++ app/src/main/java/com/home/vod/activity/NavigationDrawerFragment.java
@@ -473,6 +473,7 @@ public class NavigationDrawerFragment extends Fragment implements GetAppMenuAsyn
         }
         // ActionBarDrawerToggle ties together the the proper interactions
         // between the navigation drawer and the action bar app icon.
+
         mDrawerToggle = new ActionBarDrawerToggle(
                 getActivity (),                    /* host Activity */
                 mDrawerLayout,                    /* DrawerLayout object */
