2961ced0861c62ba1c1c6e1495885ae826eb047e
Muvidevlopers <>
2018-04-20 13:14:44 +0530

    @author Kushal
    Lumire- Sony replica server

--- app/src/lumiere/java/com.home.vod/EpisodeListOptionMenuHandler.java
+++ app/src/lumiere/java/com.home.vod/EpisodeListOptionMenuHandler.java
@@ -14,22 +14,14 @@ import com.home.vod.util.FeatureHandler;
 
 import static com.home.vod.preferences.LanguagePreference.BTN_REGISTER;
 import static com.home.vod.preferences.LanguagePreference.DEFAULT_BTN_REGISTER;
-import static com.home.vod.preferences.LanguagePreference.DEFAULT_HAS_FAVORITE;
 import static com.home.vod.preferences.LanguagePreference.DEFAULT_LANGUAGE_POPUP_LANGUAGE;
-import static com.home.vod.preferences.LanguagePreference.DEFAULT_LANGUAGE_POPUP_LOGIN;
 import static com.home.vod.preferences.LanguagePreference.DEFAULT_LOGIN;
 import static com.home.vod.preferences.LanguagePreference.DEFAULT_LOGOUT;
-import static com.home.vod.preferences.LanguagePreference.DEFAULT_MY_DOWNLOAD;
-import static com.home.vod.preferences.LanguagePreference.DEFAULT_MY_FAVOURITE;
 import static com.home.vod.preferences.LanguagePreference.DEFAULT_PROFILE;
 import static com.home.vod.preferences.LanguagePreference.DEFAULT_PURCHASE_HISTORY;
-import static com.home.vod.preferences.LanguagePreference.HAS_FAVORITE;
 import static com.home.vod.preferences.LanguagePreference.LANGUAGE_POPUP_LANGUAGE;
-import static com.home.vod.preferences.LanguagePreference.LANGUAGE_POPUP_LOGIN;
 import static com.home.vod.preferences.LanguagePreference.LOGIN;
 import static com.home.vod.preferences.LanguagePreference.LOGOUT;
-import static com.home.vod.preferences.LanguagePreference.MY_DOWNLOAD;
-import static com.home.vod.preferences.LanguagePreference.MY_FAVOURITE;
 import static com.home.vod.preferences.LanguagePreference.PROFILE;
 import static com.home.vod.preferences.LanguagePreference.PURCHASE_HISTORY;
 import static player.utils.Util.DEFAULT_IS_CHROMECAST;
@@ -55,8 +47,8 @@ public class EpisodeListOptionMenuHandler {
     private int PROFILE_INDEX = 3;
     private int PURCHASE_INDEX = 4;
     private int LOGOUT_INDEX = 5;
-    MenuItem filter_menu, profile_menu, purchage_menu, logout_menu,
-            login_menu, register_menu, mydownload_menu, favorite_menu, mediaRouteMenuItem, menu_language, action_searchmenu, submenu;
+    private MenuItem filter_menu, profile_menu, purchage_menu, logout_menu,
+            login_menu, register_menu, mediaRouteMenuItem, menu_language, action_searchmenu, submenu;
 //
 
 
@@ -74,13 +66,12 @@ public class EpisodeListOptionMenuHandler {
         String loggedInStr = preferenceManager.getLoginStatusFromPref();
         int isLogin = preferenceManager.getLoginFeatureFromPref();
 
-        MenuItem filter_menu, profile_menu, purchage_menu, logout_menu,
-                login_menu, register_menu, mydownload_menu, favorite_menu, mediaRouteMenuItem, menu_language, action_searchmenu, submenu;
+
         filter_menu = menu.findItem(R.id.action_filter);
         filter_menu.setVisible(false);
         login_menu = menu.findItem(R.id.action_login);
         menu_language = menu.findItem(R.id.menu_item_language);
-       // (menu.findItem(R.id.menu_item_language)).setVisible(false);
+        login_menu = menu.findItem(R.id.action_login);
         profile_menu = menu.findItem(R.id.menu_item_profile);
         purchage_menu = menu.findItem(R.id.action_purchage);
         logout_menu = menu.findItem(R.id.action_logout);
@@ -111,6 +102,7 @@ public class EpisodeListOptionMenuHandler {
         visibility = new boolean[6];
 
         action_searchmenu.setVisible(true);
+        purchage_menu.setVisible(false);
         filter_menu.setVisible(false);
         if ((featureHandler.getFeatureStatus(FeatureHandler.CHROMECAST, FeatureHandler.DEFAULT_CHROMECAST)))
             mediaRouteMenuItem.setVisible(true);
@@ -139,18 +131,18 @@ public class EpisodeListOptionMenuHandler {
             visibility[PROFILE_INDEX] = true;
 
 
-            if ((featureHandler.getFeatureStatus(FeatureHandler.HAS_FAVOURITE, FeatureHandler.DEFAULT_HAS_FAVOURITE)))
+           /* if ((featureHandler.getFeatureStatus(FeatureHandler.HAS_FAVOURITE, FeatureHandler.DEFAULT_HAS_FAVOURITE)))
                 favorite_menu.setVisible(true);
             else
                 favorite_menu.setVisible(false);
-
+*/
             purchage_menu.setVisible(true);
             logout_menu.setVisible(true);
             // Kushal
             visibility[PURCHASE_INDEX] = true;
             visibility[LOGOUT_INDEX] = true;
 
-            if ((featureHandler.getFeatureStatus(FeatureHandler.IS_OFFLINE, FeatureHandler.DEFAULT_IS_OFFLINE)))
+            /*if ((featureHandler.getFeatureStatus(FeatureHandler.IS_OFFLINE, FeatureHandler.DEFAULT_IS_OFFLINE)))
                 mydownload_menu.setVisible(true);
             else
                 mydownload_menu.setVisible(false);
@@ -192,8 +184,8 @@ public class EpisodeListOptionMenuHandler {
             profile_menu.setVisible(false);
             purchage_menu.setVisible(false);
             logout_menu.setVisible(false);
-            mydownload_menu.setVisible(false);
-            favorite_menu.setVisible(false);
+           /* mydownload_menu.setVisible(false);
+            favorite_menu.setVisible(false);*/
             //Kushal
             visibility[PROFILE_INDEX] = false;
             visibility[PURCHASE_INDEX] = false;
@@ -209,8 +201,8 @@ public class EpisodeListOptionMenuHandler {
         profile_menu.setVisible(false);
         purchage_menu.setVisible(false);
         logout_menu.setVisible(false);
-        mydownload_menu.setVisible(false);
-        favorite_menu.setVisible(false);
+       /* mydownload_menu.setVisible(false);
+        favorite_menu.setVisible(false);*/
         menu_language.setVisible(false);
     }
 }
