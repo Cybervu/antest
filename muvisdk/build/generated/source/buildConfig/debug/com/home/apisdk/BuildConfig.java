75b6eb3212ed2ea5e782b3f1621c782ab68a61be
Kushal <kushal@muvi.com>
2018-04-25 20:28:55 +0530

    @author Kushal onyx Push

---
+++ muvisdk/build/generated/source/buildConfig/debug/com/home/apisdk/BuildConfig.java
@@ -0,0 +1,16 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.home.apisdk;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.home.apisdk";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Fields from default config.
+  public static final String AUTH_TOKEN = "4298851905443517c9f161e6b0471969";
+  public static final String SERVICE_BASE_PATH = "https://www.muvi.com/rest/";
+}
