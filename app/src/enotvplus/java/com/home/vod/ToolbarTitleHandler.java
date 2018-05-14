f1a81bdf2f7b120e79c5020a6ae8cd3ef1c2a3d1
BISHAL <bishalseth2012@gmail.com>
2018-05-14 15:32:50 +0530

    @Author:Bishal status:Complete Desc:enoTv plus flavour added

---
+++ app/src/enotvplus/java/com/home/vod/ToolbarTitleHandler.java
@@ -0,0 +1,33 @@
+package com.home.vod;
+
+
+import android.app.Activity;
+import android.view.View;
+
+import com.home.vod.activity.MainActivity;
+import com.home.vod.activity.PPvPaymentInfoActivity;
+
+/**
+ * Created by Android on 1/10/2018.
+ */
+
+public class ToolbarTitleHandler {
+    Activity activity;
+
+
+    public ToolbarTitleHandler(Activity activity){
+        this.activity=activity;
+        try {
+            if (activity instanceof MainActivity){
+                ((MainActivity)activity).getSupportActionBar().setTitle(R.string.app_name);
+                ((MainActivity)activity).toolbarimage.setVisibility(View.GONE);
+            }
+            if (activity instanceof PPvPaymentInfoActivity){
+                ((PPvPaymentInfoActivity)activity).getSupportActionBar().setTitle(R.string.app_name);
+                ((PPvPaymentInfoActivity)activity).toolbarimage.setVisibility(View.GONE);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
