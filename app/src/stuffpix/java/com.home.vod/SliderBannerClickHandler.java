716d1ade4ccfa1cfe24f9ab50758c4e0d5659015
BISHAL <bishalseth2012@gmail.com>
2018-05-25 16:35:50 +0530

    @Author:Bishal status:completed desc:1-Stuffpix and muvinow banner link correction as this is now open in videolist fragment if we click to the banner

--- app/src/stuffpix/java/com.home.vod/SliderBannerClickHandler.java
+++ app/src/stuffpix/java/com.home.vod/SliderBannerClickHandler.java
@@ -4,9 +4,13 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
 
 import com.daimajia.slider.library.SliderTypes.BaseSliderView;
 import com.home.vod.activity.LoginActivity;
+import com.home.vod.activity.MainActivity;
+import com.home.vod.fragment.VideosListFragment;
 
 /**
  * Created by BISHAL on 17-05-2018.
@@ -18,8 +22,14 @@ public class SliderBannerClickHandler {
         this.context=context;
     }
     public void handleClickOnBanner(){
-        Intent bannerviewintent =new Intent("android.intent.action.VIEW",
+        /*Intent bannerviewintent =new Intent("android.intent.action.VIEW",
                 Uri.parse("https://www.stuffpix.co.nz/new-pix "));
-        context.startActivity(bannerviewintent);
+        context.startActivity(bannerviewintent);*/
+        Fragment fragment = new VideosListFragment();
+        Bundle bundle = new Bundle();
+        bundle.putString("title","New Pix");
+        bundle.putString("item", "new-pix");
+        fragment.setArguments(bundle);
+        ((MainActivity) context ).getSupportFragmentManager().beginTransaction().replace(R.id.container,fragment).commit();
     }
 }
